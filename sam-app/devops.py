10c211df032bc31e6b64f42b05c6fdeff7a00f2e
KaramveerHooda28 <124893357+KaramveerHooda28@users.noreply.github.com>
2024-03-02 12:42:20 -0500

    test

--- sam-app/devops.py
+++ sam-app/devops.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, this is a testing devops pipeline '
+    return 'Hello, this is a testing devops '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
